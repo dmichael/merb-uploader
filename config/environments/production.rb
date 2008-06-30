64a3d06e8432e90b5bf080badc0320acfa60f0fb
David M Michael <david@unnature.net>
2008-06-30 12:03:27 -0400

    changed to logging level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Merb.logger.info("Loaded PRODUCTION Environment...")
 Merb::Config.use { |c|
   c[:exception_details] = false
   c[:reload_classes] = false
-  c[:log_level] = :error
+  c[:log_level] = :debug
   c[:log_file] = Merb.log_path + "/production.log"
 }
 FILE_STORAGE = :s3 
